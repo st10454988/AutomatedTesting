2aa6c2d8b70193841ccd69fde645257a94804caa
st10454988 <161336309+st10454988@users.noreply.github.com>
2024-05-17 05:16:11 -0700

    Add files via upload

---
+++ src/main/java/com/mycompany/automatedunittesting/AutomatedunitTesting.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.automatedunittesting;
+
+/**
+ *
+ * @author hloso
+ */
+public class AutomatedunitTesting {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/mycompany/automatedunittesting/UnitTestingTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.automatedunittesting;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author hloso
+ */
+public class UnitTestingTest {
+    
+    public UnitTestingTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
